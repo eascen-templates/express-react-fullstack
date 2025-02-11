1e15930c11150c8970f92b1dbc3565bce1741850
eascen4 <eascen4official@gmail.com>
2025-02-10 20:34:20 -0600

    Added Dockerfile

--- server/server.ts
+++ server/server.ts
@@ -1,1 +1,26 @@
-console.log('Hello World');
+import { config } from "dotenv";
+import path from "path";
+import express from "express";
+
+config({ path: ".env" });
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/api", (request, response) => {
+    response.json({ message: "Hello World" });
+});
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.resolve(__dirname, '../../client/dist')));
+    app.get('*', (_req, res) => {
+      res.sendFile(path.resolve(__dirname, '../../client/dist/index.html'));
+    });
+  }
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
